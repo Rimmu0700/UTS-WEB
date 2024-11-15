ff2958dd428057c850ff0a26552e13a284ce2ed9
Rimmu0700 <armeza07@gmail.com>
2024-11-15 22:38:56 +0800

    Delete UTS WEB directory
    ganti

--- UTS WEB/ganti.js
+++
@@ -1,9 +0,0 @@
-const image = document.querySelector('.Foto');
-
-image.addEventListener('mouseover', () => {
-    image.src = 'Assets/Acer/Acer2.jpg'; // Ganti gambar ke 2
-});
-
-image.addEventListener('mouseout', () => {
-    image.src = 'Assets/Acer/Acer Nitro 15.png'; // Ganti gambar ke 1
-});
